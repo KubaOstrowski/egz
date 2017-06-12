8653542672f15dead910801efa47c66b4dd91d73
KubaOstrowski <ostrowskikuba@vp.pl>
2017-06-12 12:33:26 +0200

    Add files via upload

---
+++ strona2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<a href="index.html"><img src="baner.jpg"/></a>
+</head>
+<body>
+<form name="action" method="get"/>
+</h1>POTĘGOWANIE</h1><br />
+<i>Podaj pierwszą liczbę:</i><input type="text" id="1"/><br /><br />
+<i>Podaj drugą liczbę:</i><input type="text" id="2"/><br /><br />
+</form>
+<input type="button" id="potengowanie" value="potengowanie"/>
+<?php
+?>
+</body>
+</html>
